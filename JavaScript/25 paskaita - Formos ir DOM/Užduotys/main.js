0980ed79d93fce6b9f5ae9f61b2a546156739016
DiogenSin <jakasdovydas@gmail.com>
2022-12-11 19:38:23 +0200

    25 paskaita užduoties UPD

--- JavaScript/25 paskaita - Formos ir DOM/Užduotys/main.js
+++ JavaScript/25 paskaita - Formos ir DOM/Užduotys/main.js
@@ -1,5 +1,5 @@
 
-//------------------------Filmų kortelės----------------------------
+//------------------------1 Filmų kortelės----------------------------
 
 document.querySelector('#form').addEventListener('submit', event => {
     event.preventDefault()
@@ -34,7 +34,7 @@ document.querySelector('#form').addEventListener('submit', event => {
 })
 
 
-//------------------------Lentelės generatorius----------------------------
+//------------------------2 Lentelės generatorius----------------------------
 
 document.querySelector('#tableGenerator').addEventListener('submit', event2 => {
     event2.preventDefault()
@@ -46,7 +46,83 @@ document.querySelector('#tableGenerator').addEventListener('submit', event2 => {
     ]
     console.log(eilutes + ' eilutės ir ' + stulpeliai + ' stulpeliai.')
 
-document.querySelector('#uzduotis2').innerHTML += '<table id="table"><h1>Sugeneruota lentelė</h1></table>'
+document.querySelector('#uzduotis2').innerHTML += '<table id="table"><h1>Sugeneruota lentelė</h1><tbody></tbody></table>'
 
+let table = document.querySelector('#table')
 
+for ( let i = 0 ; i < eilutes ; i++){
+    row = document.createElement('tr')
+    table.append(row)
+   for( j = 0 ; j < stulpeliai ; j++){
+    row.insertCell() 
+   }
+}
 })
+
+//-------------3 Filmų duomenų bazė------------------
+
+
+let movieCards = document.querySelector('#uzduotis3')
+
+movieCards.addEventListener('submit', event3 => {
+
+    event3.preventDefault()
+
+    console.dir(event3)
+
+    let pasirinktasTipas = event3.target.elements.filtras.value
+    console.log(pasirinktasTipas)
+
+    for ( i = 0 ; i < data.movies.length ; i++)
+{
+
+    let filtracijaPagalPasirinkima = data.movies[i].tipas.some(tipas => tipas == pasirinktasTipas)
+
+    console.log(filtracijaPagalPasirinkima)
+
+    if (filtracijaPagalPasirinkima == true){
+    movieCards.innerHTML += 
+    `
+<section class="movieCard">
+    <div class="image">
+        <img src="${data.movies[i].paveiksliukas}" alt="Pateiktas paveikslas">
+        <div class="dataLocation">
+            <p>${data.movies[i].leidimoMetai}</p>
+            <p>${data.movies[i].IMDB} IMDB</p>
+        </div>
+    </div>
+    <div class="text">
+        <h2>${data.movies[i].pavadinimas}</h2>
+        <p>${data.movies[i].aprasymas}</p>
+        <p>
+        <b>Tipas:</b> ${data.movies[i].tipas}<br>
+        <b>Trukmė:</b> ${data.movies[i].trukme} min.<br>
+        <b>Režisierius:</b> ${data.movies[i].rezisierius}<br>
+        <b>Aktoriai:</b> ${data.movies[i].aktoriai}
+        </p>
+    </div>
+</section>
+
+`
+}
+};
+})
+
+//-------------4 Filmų filtras------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
